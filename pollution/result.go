367ad7e9df540719495c24091ae72b1f23f7ee16
Lucien Brun <lucien.brun@epitech.eu>
2021-02-01 13:24:57 +0100

    Display result by adding binomial coeff and bernstein polynomial

--- pollution/result.go
+++ pollution/result.go
@@ -19,29 +19,38 @@ var (
 	Points []Point
 )
 
-func bezierSurface() float64 {
+func buildMatrix() [][]int64 {
+	matrix := make([][]int64, N)
+	for i := 0; int64(i) < N; i++ {
+		matrix[i] = make([]int64, N)
+	}
+
+	for _, point := range Points {
+		matrix[point.X][point.Y] = point.P
+	}
+	return matrix
+}
+
+func bezierSurface(matrix [][]int64) float64 {
 	n := N - 1
 	x := X / float64(n)
 	y := Y / float64(n)
 	res := 0.0
 
 	for i := int64(0); i < n + 1; i++ {
-		for j := int64(0); j < n + 1; i++ {
-			bernX := utils.GetBinomial(n, i, x)
-			bernY := utils.GetBinomial(n, j, y)
-
-			res += bernX * bernY * float64(Points[i].P)
+		for j := int64(0); j < n + 1; j++ {
+			bernX := utils.BernsteinPolynomial(uint64(n), uint64(i), x)
+			bernY := utils.BernsteinPolynomial(uint64(n), uint64(j), y)
+			res += bernX * bernY * float64(matrix[i][j])
 		}
 	}
 	return res
 }
 
-func displayResult() {
-	fmt.Printf("%.2f", bezierSurface())
-}
-
 // Main - 309pollution main
 func Main() error {
-	displayResult()
+	matrix := buildMatrix()
+	result := bezierSurface(matrix)
+	fmt.Printf("%.2f\n", result)
 	return nil
 }
--- utils/binomial.go
+++ utils/binomial.go
@@ -1,59 +1,24 @@
 package utils
 
 import (
-	"fmt"
-	"math/big"
-	"os"
+	"math"
 )
 
-// Factorial - Get factorial big int
-func Factorial(x *big.Int) *big.Int {
-	result := big.NewInt(1)
-	i := big.NewInt(2)
-
-	if !x.IsInt64() {
-		fmt.Println("The number is way too big to calculate a factorial")
-		os.Exit(84)
+func Factorial(n uint64)(result uint64) {
+	if n > 0 {
+		result = n * Factorial(n - 1)
+		return result
 	}
-	for i.Cmp(x) != 1 {
-		result.Mul(result, i)
-		i = i.Add(i, big.NewInt(1))
-	}
-	return result
+	return 1
 }
 
-func getBinomialCoefficient(n *big.Int, k *big.Int) *big.Int {
-	if k.Cmp(n) == 1 {
-		fmt.Println("Error: k > n")
-		os.Exit(84)
-	}
-
-	numerator := Factorial(n)
-	subNK := big.NewInt(1).Sub(n, k)
-	denominator := big.NewInt(1).Mul(Factorial(k), Factorial(subNK))
-	res := big.NewInt(1).Div(numerator, denominator)
-	return res
+func BinomialCoefficient(n uint64, i uint64) float64 {
+	return float64(Factorial(n) / (Factorial(i) * Factorial(n - i)))
 }
 
-// BigPow - big Float
-func BigPow(a *big.Float, e int64) *big.Float {
-	if e == 0 {
-		return big.NewFloat(1.0)
-	}
-	result := big.NewFloat(0.0).Copy(a)
-	for i := int64(0); i < e - 1; i++ {
-		result = result.Mul(result, a)
-	}
-	return result
-}
-
-// GetBinomial - get binomial
-func GetBinomial(n int64, k int64, p float64) float64 {
-	res := big.NewFloat(0.0).Mul(
-		big.NewFloat(0.0).SetInt(getBinomialCoefficient(big.NewInt(0.0).SetInt64(n), big.NewInt(0.0).SetInt64(k))),
-		BigPow(big.NewFloat(0.0).SetFloat64(p), k))
-	res.Mul(res, BigPow(big.NewFloat(0.0).Sub(big.NewFloat(1.0), big.NewFloat(0.0).SetFloat64(p)), n - k))
-
-	resConverted, _ := res.Float64()
-	return resConverted
+func BernsteinPolynomial(n uint64, i uint64, u float64) float64 {
+	a := BinomialCoefficient(n, i)
+	b := math.Pow(u, float64(i))
+	c := math.Pow(1 - u, float64(n - i))
+	return a * b * c
 }
